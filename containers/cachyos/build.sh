0a5d419dda53a822829cdde083f00b24f123091f
Anders Rytter Hansen <andersr+github@rytter.me>
2024-02-28 10:01:00 +0100

    remove containers

--- containers/cachyos/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-podman pull docker.io/cachyos/cachyos-v3
-podman build -t andersrh/cachyos/base-gui -f base-gui.Dockerfile .
--- containers/cachyos/general/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-APPNAME="general"
-
-podman build -t andersrh/cachyos/$APPNAME -f cachyos.Dockerfile .
-podman rm -f $APPNAME-cachyos
-distrobox create --image andersrh/cachyos/$APPNAME --home ~/containers/$APPNAME/ $APPNAME-cachyos
-
-distrobox enter $APPNAME-cachyos -- distrobox-export --app codium
