f4aa78503bdedede93cd0c21192bd0e1a7e793a4
Anders Rytter Hansen <andersrh@users.noreply.github.com>
2025-10-07 17:37:48 -0300

    remove more unused files from old fedora image

--- akmods.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-KERNEL=$1
-
-KERNEL_VERSION="$(rpm -q $KERNEL --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
-
-akmods --force --kernels "${KERNEL_VERSION}"
--- install_cachyos_kernel.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rpm-ostree override remove kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra --install $1
